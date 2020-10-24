9b1310c2c30fc5521fd1c7805edaccbd9dc0cd8c
Vladislove <vladislav.lapko@gmail.com>
2020-10-25 00:58:00 +0200

    First commit

---
+++ Models/People.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace PeopleApi.Models
+{
+    public class People
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+}
