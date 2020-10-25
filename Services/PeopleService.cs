8eb3b999e3347f99aa92db0e0574cb468dc3a9a4
Vladislove <vladislav.lapko@gmail.com>
2020-10-25 12:31:33 +0100

    Added: peopleService, changed startup

---
+++ Services/PeopleService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using PeopleApi.Models;
+
+namespace PeopleApi.Services
+{
+    public class PeopleService
+    {
+        private readonly IMongoCollection<People> _people;
+
+        public PeopleService(IPeopleDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _people = database.GetCollection<People>(settings.PeopleCollectionName);
+        }
+
+        public List<People> Get() => _people.Find(p => true).ToList();
+
+        public People Get(string id) => _people.Find<People>(p => p.Id == id).FirstOrDefault();
+
+        public People Create(People p)
+        {
+            _people.InsertOne(p);
+            return p;
+        }
+
+        public void Update(string id, People p) => _people.ReplaceOne(person => person.Id == id, p);
+        public void Remove(People p) => _people.DeleteOne(person => person.Id == p.Id);
+        public void Remove(string id) => _people.DeleteOne(person => person.Id == id);
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using PeopleApi.Models;
 
 namespace PeopleApi
 {
@@ -25,6 +27,8 @@ namespace PeopleApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<PeopleDatabaseSettings>(Configuration.GetSection(nameof(PeopleDatabaseSettings)));
+            services.AddSingleton<IPeopleDatabaseSettings>(sp => sp.GetRequiredService<IOptions<PeopleDatabaseSettings>>().Value);
             services.AddControllers();
         }
 
